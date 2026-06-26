839c6a582bd9638a79ee1f1035fc206652fd3248
unknown <6tailsfox777@gmail.com>
2026-06-26 13:45:12 +0800

    123456

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class SensorReading(models.Model):
+    value = models.FloatField(verbose_name="感測器數值")
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="記錄時間")
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return f"{self.timestamp} - {self.value}"
